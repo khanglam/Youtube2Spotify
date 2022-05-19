ca8ee8e93dbc4d576fb8ef239f1972de55a7261c
khanglam <KLam@mercuryinsurance.com>
2022-05-18 23:48:58 -0700

    Fetch User/Password from App to Create Entries DB
    User Authentication

--- flask-server/server/forms.py
+++ flask-server/server/forms.py
@@ -1,6 +1,8 @@
+from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from server.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -12,6 +14,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different username.')
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('That email is taken. Please choose a different email.')
 
 
 class LoginForm(FlaskForm):
