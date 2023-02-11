568758f768a89a14050b97492dc6e6193e905bef
Khang Lam <khangtlam@gmail.com>
2023-02-10 19:24:33 -0800

    Restructure release and ready for deployment

---
+++ server/components/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from components.config import ApplicationConfig
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config.from_object(ApplicationConfig) # Get config from config.py file
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+CORS(app, supports_credentials=True)
+# CORS(app, supports_credentials=True, resources={r"*": {"origins": "*"}}) # Allow Cross Origin
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info' # Flash message for 'Login to access this page'
+
+from components import routes
