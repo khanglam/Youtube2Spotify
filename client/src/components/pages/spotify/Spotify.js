3cbc518c98c13e04aaf55e7956d0ef6a02d835fd
khanglam <KLam@mercuryinsurance.com>
2023-02-16 00:36:40 -0800

    Removed auto redirect when response failed

--- client/src/components/pages/spotify/Spotify.js
+++ client/src/components/pages/spotify/Spotify.js
@@ -1,13 +1,13 @@
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect } from 'react';
 
-import { Container } from "react-bootstrap";
-import Axios from "../../Axios";
-import StreamMusic from "./StreamMusic";
-import { TokenInfo } from "./TokenInfo";
+import { Container } from 'react-bootstrap';
+import Axios from '../../Axios';
+import StreamMusic from './StreamMusic';
+import { TokenInfo } from './TokenInfo';
 
-const LOGIN_URL = "/loginSpotify";
-const REFRESH_TOKEN_URL = "/refreshSpotifyToken";
-const CLEAR_SPOTIFY_CACHE = "/clearSpotifyCache";
+const LOGIN_URL = '/loginSpotify';
+const REFRESH_TOKEN_URL = '/refreshSpotifyToken';
+const CLEAR_SPOTIFY_CACHE = '/clearSpotifyCache';
 
 function Spotify() {
   const [tokenInfo, setTokenInfo] = useState();
@@ -23,20 +23,20 @@ function Spotify() {
     try {
       const response = await Axios.get(LOGIN_URL);
       setTokenInfo(response.data);
-      setAccessToken(response.data["access_token"]);
-      setRefreshToken(response.data["refresh_token"]);
-      setExpiresIn(response.data["expires_in"]);
+      setAccessToken(response.data['access_token']);
+      setRefreshToken(response.data['refresh_token']);
+      setExpiresIn(response.data['expires_in']);
     } catch (error) {
       console.log(error);
-      window.location = "/Spotify";
+      // window.location = "/Spotify";
     }
   };
 
   async function clearSpotifyCache() {
     try {
       const response = await Axios.get(CLEAR_SPOTIFY_CACHE);
-      console.log("Spotify Cache Cleared");
-      window.location = "/Spotify";
+      console.log('Spotify Cache Cleared');
+      window.location = '/Spotify';
     } catch (error) {
       console.log(error);
     }
@@ -50,10 +50,10 @@ function Spotify() {
         try {
           const response = await Axios.get(REFRESH_TOKEN_URL);
           setTokenInfo(response.data);
-          setAccessToken(response.data["access_token"]);
-          setRefreshToken(response.data["refresh_token"]);
-          setExpiresIn(response.data["expires_in"]);
-          console.log("Token Refreshed");
+          setAccessToken(response.data['access_token']);
+          setRefreshToken(response.data['refresh_token']);
+          setExpiresIn(response.data['expires_in']);
+          console.log('Token Refreshed');
         } catch (error) {
           console.log(error);
         }
@@ -70,26 +70,26 @@ function Spotify() {
       {tokenInfo ? (
         <Container // Initial Login Page
           style={{
-            minHeight: "90vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
+            minHeight: '90vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
           }}
         >
           {streamMusic ? (
             <TokenInfo.Provider value={tokenInfo}>
               <Container // Stream Music Page
-                className="d-flex justify-content-center"
+                className='d-flex justify-content-center'
               >
                 <input
-                  type="button"
-                  className="btn btn-success"
-                  value="Back"
+                  type='button'
+                  className='btn btn-success'
+                  value='Back'
                   style={{
-                    maxHeight: "38px",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    textAlign: "center",
+                    maxHeight: '38px',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textAlign: 'center'
                   }}
                   onClick={() => {
                     setStreamMusic(false);
@@ -101,25 +101,25 @@ function Spotify() {
           ) : (
             <Container // Menu Page
               style={{
-                minHeight: "90vh",
-                display: "flex",
-                justifyContent: "space-evenly",
-                alignItems: "center",
-                flexDirection: "column",
+                minHeight: '90vh',
+                display: 'flex',
+                justifyContent: 'space-evenly',
+                alignItems: 'center',
+                flexDirection: 'column'
               }}
             >
               <input
-                value="Stream Music"
-                type="button"
-                className="btn btn-success btn-lg"
+                value='Stream Music'
+                type='button'
+                className='btn btn-success btn-lg'
                 onClick={() => {
                   setStreamMusic(true);
                 }}
               ></input>
               <input
-                value="Clear Cache"
-                type="button"
-                className="btn btn-danger btn-lg"
+                value='Clear Cache'
+                type='button'
+                className='btn btn-danger btn-lg'
                 onClick={() => {
                   clearSpotifyCache();
                 }}
@@ -129,18 +129,18 @@ function Spotify() {
         </Container>
       ) : (
         <Container
-          className="d-flex justify-content-center align-items-center"
+          className='d-flex justify-content-center align-items-center'
           style={{
-            minHeight: "90vh",
+            minHeight: '90vh'
           }}
         >
           <input
             onClick={handleSubmit}
-            className="btn btn-success btn-lg"
-            id="submit"
-            name="submit"
-            type="submit"
-            value="Login With Spotify"
+            className='btn btn-success btn-lg'
+            id='submit'
+            name='submit'
+            type='submit'
+            value='Login With Spotify'
           ></input>
         </Container>
       )}
