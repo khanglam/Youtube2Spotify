6992642314bfa7ec911d31156e830122c2a0453e
khanglam <KLam@mercuryinsurance.com>
2023-02-26 22:11:22 -0800

    Completed: Youtube Oauth2 new implementation. Works on localhost

--- server/run.py
+++ server/run.py
@@ -3,12 +3,8 @@ import os
 port = int(os.environ.get('PORT', 5000))
 print("Port: ", port)
 
-
 if __name__ == '__main__':
     # When running locally, disable OAuthlib's HTTPs verification.
-    # ACTION ITEM for developers:
-    #     When running in production *do not* leave this option enabled.
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-
-
+    # ACTION ITEM - When running in production *do not* leave this option enabled.
+    # os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     app.run(port=port)
